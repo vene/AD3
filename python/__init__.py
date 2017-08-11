1646b2a097ed24ed34ad204ef94403791580a51d
vene <vlad@vene.ro>
2017-08-11 13:44:15 -0400

    expose solve_qp function

--- python/__init__.py
+++
@@ -1,4 +0,0 @@
-__version__ = '2.1.dev0'
-
-from .factor_graph import PBinaryVariable, PFactorGraph, PMultiVariable
-from .simple_inference import simple_grid, general_graph
---
+++ python/ad3/__init__.py
@@ -0,0 +1,5 @@
+__version__ = '2.1.dev0'
+
+from .base import PBinaryVariable, PMultiVariable
+from .factor_graph import PFactorGraph
+from .simple_inference import simple_grid, general_graph
